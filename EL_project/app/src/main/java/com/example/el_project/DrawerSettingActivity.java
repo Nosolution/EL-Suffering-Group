472cdafac7784d2517a387e687336996a5370c2d
NAiveD <nice-die@live.com>
2018-04-22 02:31:43 +0800

    一点小东西：实现了把设置侧栏和本地设置储存链接起来，开始在写TaskOngoing的活动 by NA

--- EL_project/app/src/main/java/com/example/el_project/DrawerSettingActivity.java
+++ EL_project/app/src/main/java/com/example/el_project/DrawerSettingActivity.java
@@ -1,5 +1,6 @@
 package com.example.el_project;
 
+import android.graphics.drawable.DrawableWrapper;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
@@ -26,8 +27,10 @@ public class DrawerSettingActivity extends AppCompatActivity implements Compound
 		mDrawerLayout=findViewById(R.id.drawer_layout);
 		ActionBar actionBar=getSupportActionBar();
 
-		switch_clock_status=findViewById(R.id.switch_clock);
-		switch_music_status=findViewById(R.id.switch_music);
+		switch_clock_status=findViewById(R.id.switch_if_tomato_clock_on);
+		switch_music_status=findViewById(R.id.switch_if_music_on);
+		switch_clock_status.setChecked(GeneralSetting.getTomatoClockEnable(this));
+		switch_music_status.setChecked(GeneralSetting.getMusicOn(this));
 		switch_clock_status.setOnCheckedChangeListener(this);
 		switch_music_status.setOnCheckedChangeListener(this);
 
@@ -59,13 +62,25 @@ public class DrawerSettingActivity extends AppCompatActivity implements Compound
 	@Override
 	public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
 		switch (compoundButton.getId()){
-			case R.id.switch_music:
-				if(compoundButton.isChecked()) Toast.makeText(this,"音乐已打开",Toast.LENGTH_SHORT).show();
-				else Toast.makeText(this,"音乐已关闭",Toast.LENGTH_SHORT).show();
+			case R.id.switch_if_music_on:
+				if(compoundButton.isChecked()) {
+				    GeneralSetting.setMusicOn(DrawerSettingActivity.this, true);
+					Toast.makeText(this,"音乐已打开",Toast.LENGTH_SHORT).show();
+				}
+				else {
+				    GeneralSetting.setMusicOn(DrawerSettingActivity.this, false);
+				    Toast.makeText(this,"音乐已关闭",Toast.LENGTH_SHORT).show();
+				}
 				break;
-			case R.id.switch_clock:
-				if(compoundButton.isChecked()) Toast.makeText(this,"番茄钟已打开",Toast.LENGTH_SHORT).show();
-				else Toast.makeText(this,"番茄钟已关闭",Toast.LENGTH_SHORT).show();
+			case R.id.switch_if_tomato_clock_on:
+				if(compoundButton.isChecked()) {
+				    GeneralSetting.setTomatoClockEnable(DrawerSettingActivity.this, true);
+				    Toast.makeText(this,"番茄钟已打开",Toast.LENGTH_SHORT).show();
+				}
+				else {
+				    GeneralSetting.setTomatoClockEnable(DrawerSettingActivity.this, false);
+				    Toast.makeText(this,"番茄钟已关闭",Toast.LENGTH_SHORT).show();
+				}
 				break;
 		}
 	}
